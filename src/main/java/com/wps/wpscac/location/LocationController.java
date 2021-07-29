ffa42461e383ee83a4c571b02aa2bfa3529bc3d4
fd95i <federicon35@gmail.com>
2021-07-28 22:37:43 -0300

    trying comment deploys

--- src/main/java/com/wps/wpscac/location/LocationController.java
+++ src/main/java/com/wps/wpscac/location/LocationController.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
