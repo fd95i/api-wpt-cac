101f760763ebd9fc5e4f2344ebd3d0d32e462645
lauramicaela-m <mendez.lauramicaela@gmail.com>
2021-07-29 13:48:27 -0300

    more client endpoints for demo

--- src/main/java/com/wps/wpscac/contact/ContactController.java
+++ src/main/java/com/wps/wpscac/contact/ContactController.java
@@ -1,6 +1,7 @@
 package com.wps.wpscac.contact;
 
 import com.wps.wpscac.contact.entity.Contact;
+import com.wps.wpscac.contact.exception.ContactNotFoundException;
 import com.wps.wpscac.contact.repository.ContactRepository;
 import com.wps.wpscac.utils.SendContactMail;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,10 +24,30 @@ public class ContactController {
         return contactRepository.findAll();
     }
 
-    @PostMapping("new")
+    @GetMapping("{id}")
+    public Contact get(@PathVariable("id") Integer id) {
+        return contactRepository
+                .findById(id)
+                .orElseThrow(ContactNotFoundException::new);
+    }
+
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void save(@RequestBody Contact c) {
         contactRepository.save(c);
         SendContactMail.dispatch(c);
     }
+
+    @PutMapping
+    public void update(@RequestBody Contact c) {
+        contactRepository.save(c);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Integer id) {
+        contactRepository.delete(contactRepository
+                .findById(id)
+                .orElseThrow(ContactNotFoundException::new));
+    }
+
 }
