0adf6dedfa259400e75ed7a1acf5434e42859cf2
fd95i <federicon35@gmail.com>
2021-08-09 23:41:28 -0300

    properties for location

--- src/main/java/com/wps/wpscac/location/entity/Location.java
+++ src/main/java/com/wps/wpscac/location/entity/Location.java
@@ -1,5 +1,6 @@
 package com.wps.wpscac.location.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,11 +17,19 @@ public class Location {
 
     @Id
     @Column(name = "location_id")
+    @JsonProperty("id_sucursal")
     private Integer locationId;
+    @JsonProperty("calle")
     private String street;
+    @JsonProperty("altura")
     private String number;
+    @JsonProperty("barrio")
     private String neighboor;
+    @JsonProperty("ciudad")
+    private String state;
     @Column(name = "zip_code")
+    @JsonProperty("codigoPostal")
     private String zipCode;
+    @JsonProperty("googleMaps")
     private String pb;
 }
